f02bccad53018c74fa5bb51355cdcb29e8ca2510
cgrevil <cgrevil@gmail.com>
2015-10-27 12:02:46 +0100

    Student og StudyProgramme klasser tilføjet

---
+++ OLProgram/OLModel/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLModel
+{
+    class Student
+    {
+        private static int StudentIdCounter = 0;
+
+        public int StudentId { get; }
+
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+
+        public StudyProgramme StudyProgramme { get; set; }
+
+        public Student() : this("", "", null) { }
+
+        public Student(string FirstName, string LastName, StudyProgramme StudyProgramme)
+        {
+            this.StudentId = StudentIdCounter++;
+            this.FirstName = FirstName;
+            this.LastName = LastName;
+            this.StudyProgramme = StudyProgramme;
+        }
+    }
+
+    class StudyProgramme
+    {
+        private static int ProgrammeIdCounter = 0;
+
+        public int ProgrammeId { get; }
+
+        public string ProgrammeName { get; set; }
+        public Boolean IsBEngProgramme { get; set; } //diplom?
+
+        public StudyProgramme(String ProgrammeName, Boolean IsBEngProgramme)
+        {
+            this.ProgrammeId = ProgrammeIdCounter++;
+            this.ProgrammeName = ProgrammeName;
+            this.IsBEngProgramme = IsBEngProgramme;
+        }
+    }
+}
--- OLProgram/OLProgram/ViewModel/MainViewModel.cs
+++ OLProgram/OLProgram/ViewModel/MainViewModel.cs
@@ -2,7 +2,7 @@ using System;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.CommandWpf;
 using System.Windows.Input;
-
+using _OLModel;
 namespace OLProgram.ViewModel
 {
     /// <summary>
