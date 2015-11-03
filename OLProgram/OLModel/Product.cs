3aca6e16f16aa14b2b546db2e29acdc3c7b3929d
cgrevil <cgrevil@gmail.com>
2015-11-03 11:11:39 +0100

    Product- and BasketItem classes added

---
+++ OLProgram/OLModel/Product.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OLModel
+{
+    class Product
+    {
+        private static int ProductIdCounter = 0;
+
+        public int ProductId { get; }
+
+        public string Name { get; set; }
+        public string ImageFileName { get; set; }
+        
+        public Product() : this("<NAME>", "default.png") { }
+
+        public Product(string Name, string ImageFileName)
+        {
+            this.ProductId = ProductIdCounter++;
+            this.Name = Name;
+            this.ImageFileName = ImageFileName;
+        }
+    }
+
+    class BasketItem
+    {
+        public int ProductId { get; }
+        public int Count { get; set; }
+
+        public BasketItem(int ProductId) : this(ProductId, 1) { }
+
+        public BasketItem(int ProductId, int Count)
+        {
+            this.ProductId = ProductId;
+            this.Count = Math.Max(Count, 1);
+        }
+    }
+}
