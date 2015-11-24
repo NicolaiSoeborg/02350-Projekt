a9a4f62abd4121eb8d1e426ce1535cbad3aed48c
NicolaiSoeborg <NicolaiSoeborg@gmail.com>
2015-11-24 15:09:45 +0100

    Bedre sikkerhed i Admin Password delen

--- OLProgram/OLProgram/ViewModel/AdminVM.cs
+++ OLProgram/OLProgram/ViewModel/AdminVM.cs
@@ -9,16 +9,17 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Input;
+using System.Security.Cryptography;
 
 
 namespace OLProgram.ViewModel
 {
     public class AdminVM : BaseVM
     {
+        private static string ADMIN_PASSWORD = "R4JrVIuzP2DEGtCkcrpX8bJUhOfmAow2wpH2XgOfL5o="; // "OLProgram"
         public string TxtAdminPassword {
             get { return ""; }
-            set { if (value != null && value.Equals("OLP")) MainWindow.Content = new View.AdminUC(); }
+            set { if (value != null && TestAdminPassword(value)) MainWindow.Content = new View.AdminUC(); }
         }
 
         public RelayCommand AddProductToGlobalCommand { get; }
@@ -33,6 +34,23 @@ namespace OLProgram.ViewModel
             AddProductToGlobalCommand = new RelayCommand(AddProductToGlobal);
             AddUserCommand = new RelayCommand(AddUser);
             CloseApplicationCommand = new RelayCommand(CloseApplication);
+        }
+
+        private bool TestAdminPassword(string pwd)
+        {
+            try
+            {
+                byte[] salt = new byte[] { 0x4F, 0x4C, 0x50, 0x72, 0x6F, 0x67, 0x72, 0x61, 0x6D }; // "OLProgram" (must be 8 bytes or larger)
+                int iterations = 10000; // default is 1000
+                Rfc2898DeriveBytes hash = new Rfc2898DeriveBytes(pwd, salt, iterations); // PBKDF2
+
+                // hash.Equals(byte[] sameBytes) => giver altid false ...
+                if (Convert.ToBase64String(hash.GetBytes(32)).Equals(ADMIN_PASSWORD))
+                    return true;
+                
+            }
+            catch { MessageBox.Show("Fejl i TestAdminPassword"); }
+            return false;
         }
 
         private void AddProductToGlobal() { } // TODO
