4ff283b963bbf109587713e3d8caae629ed0f6f4
NicolaiSoeborg <NicolaiSoeborg@gmail.com>
2015-12-01 17:19:07 +0100

    Lille optimering

--- OLProgram/OLProgram/ViewModel/BasketVM.cs
+++ OLProgram/OLProgram/ViewModel/BasketVM.cs
@@ -46,17 +46,9 @@ namespace OLProgram.ViewModel
 
         }
 
+        private Dictionary<Key, char> num2Char = new Dictionary<Key, char> { { Key.D0, '0' }, { Key.D1, '1' }, { Key.D2, '2' }, { Key.D3, '3' }, { Key.D4, '4' }, { Key.D5, '5' }, { Key.D6, '6' }, { Key.D7, '7' }, { Key.D8, '8' }, { Key.D9, '9' } };
         private void writeInput(KeyEventArgs e)
         {
-            Dictionary<Key, char> num2Char = new Dictionary<Key, char>
-            {
-                { Key.D0, '0' }, { Key.D1, '1' },
-                { Key.D2, '2' }, { Key.D3, '3' },
-                { Key.D4, '4' }, { Key.D5, '5' },
-                { Key.D6, '6' }, { Key.D7, '7' },
-                { Key.D8, '8' }, { Key.D9, '9' }
-            };
-
             if (e.Key == Key.Enter)
                 enterInput();
             else
