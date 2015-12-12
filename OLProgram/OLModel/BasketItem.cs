7341b91431e78a21965cdbbbfbb06e9fbdc42020
SilasPihl <pihl92@hotmail.com>
2015-12-12 15:22:36 +0100

    Lavet udseendet om i basketitemUC
    Antallet er i mellem minus og plus nu :)

--- OLProgram/OLModel/BasketItem.cs
+++ OLProgram/OLModel/BasketItem.cs
@@ -4,9 +4,13 @@ namespace OLModel
 {
     public class BasketItem : Product
     {
-        public int Count { get; set; }
-
+        private int _count;
+        private string _name;
         private String _countAndName;
+
+        public int Count { get { return _count; } set { _count = value; NotifyPropertyChanged(); } }
+        public string Name { get { return _name; } set { _name = value; NotifyPropertyChanged(); } }
+
         public String CountAndName { get { return _countAndName; } set { _countAndName = value; NotifyPropertyChanged(); } }
 
         public BasketItem(Product product) : this(product, 1) { }
@@ -14,6 +18,7 @@ namespace OLModel
         public BasketItem(Product product, int amount) : base(product)
         {
             Count = Math.Max(amount, 1);
+            Name = product.ProductName;
             CountAndName = String.Format("{0} x {1}", Count, product.ProductName);
         }
 
