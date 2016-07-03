14cd5746b648c48401621f4010d42cc7d40ba685
Nicolai <NicolaiSoeborg@users.noreply.github.com>
2016-07-03 14:44:22 +0200

    Refactor SQL logic in Model

--- OLModel/Model.cs
+++ OLModel/Model.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Data.SQLite;
 
 namespace OLModel
@@ -13,15 +14,18 @@ namespace OLModel
         public List<String> AdminLog { get; set; }
         public List<String> UserLog { get; set; }
 
-        public static int MODEL_VERSION = 1;
-        private static int LOG_ADMIN_ONLY = 1; // everything <= is admin only (see note below)
+        public const int MODEL_VERSION = 1;
+        private const int LOG_ADMIN_ONLY = 1; // everything <= is admin only (see note below)
 
-        private void createNewDatabase(string filename)
+        private SQLiteConnection dbConn;
+        private const string dbFilename = "testdb.sqlite";
+        private const string connStr = "Data Source={0};Version=3;";
+
+        private bool createNewDatabase()
         {
-            Console.WriteLine(String.Format("Creating database \"{0}\"", filename));
-            SQLiteConnection.CreateFile(filename);
-            string connStr = String.Format("Data Source={0};Version=3;", filename);
-            SQLiteConnection dbConn = new SQLiteConnection(connStr);
+            Console.WriteLine(String.Format("Creating database \"{0}\"", dbFilename));
+            SQLiteConnection.CreateFile(dbFilename);
+            dbConn = new SQLiteConnection(String.Format(connStr, dbFilename));
             dbConn.Open();
             string[] statements = {
                 "CREATE TABLE IF NOT EXISTS users(studentID TEXT NOT NULL PRIMARY KEY, studentName TEXT);",
@@ -42,11 +46,13 @@ namespace OLModel
                 command.ExecuteNonQuery();
             }
             //dbConn.Commit();
-            dbConn.Close();
+            return true;
         }
 
-        private int getVersion (SQLiteConnection dbConn)
+        private int getVersion()
         {
+            if (!OpenConnection()) return -1;
+
             SQLiteCommand command = new SQLiteCommand("SELECT val FROM settings WHERE key = 'version' LIMIT 1", dbConn);
             SQLiteDataReader reader = command.ExecuteReader();
 
@@ -55,25 +61,32 @@ namespace OLModel
                 int version = reader.GetInt32(0);
                 /*if (!Int32.TryParse(reader["version"].ToString(), out version))
                     return -1;*/
-
                 return version;
             }
             return -1;
         }
 
-        private Model() {
-            string filename = "testdb.sqlite3";
-            if (!System.IO.File.Exists(filename))
-                createNewDatabase(filename);
+        private bool OpenConnection()
+        {
+            if (!System.IO.File.Exists(dbFilename))
+                return createNewDatabase();
 
-            string connStr = String.Format("Data Source={0};Version=3;", filename);
-            SQLiteConnection dbConn = new SQLiteConnection(connStr);
+            if (dbConn != null && dbConn.State == ConnectionState.Open)
+                return true; // Connection already open?
+
+            dbConn = new SQLiteConnection(String.Format(connStr, dbFilename));
             dbConn.Open();
 
-            int version = getVersion(dbConn);
+            return true;
+        }
+
+        private Model() {
+            if (!OpenConnection()) return; // TODO: Kill?
+
+            int version = getVersion();
             if (version != MODEL_VERSION)
             {
-                Console.WriteLine(String.Format("Unknown database version {0} for file \"{1}\"", version, filename));
+                Console.WriteLine(String.Format("Unknown database version {0} for file \"{1}\"", version, dbFilename));
                 return; // TODO: Kill?
             }
 
@@ -166,6 +179,26 @@ namespace OLModel
             }
             #endregion
 
+        }
+
+        ~Model()
+        {
+            #region unneeded
+            /*Console.WriteLine(String.Format("{0} - Model destructor called.", Helpers.getTimeStamp()));
+            if (dbConn != null)
+            {
+                while (dbConn.State == ConnectionState.Fetching
+                    || dbConn.State == ConnectionState.Executing)
+                {
+                    Console.WriteLine(String.Format("{0} - Waiting for db connection.", Helpers.getTimeStamp()));
+                    System.Threading.Thread.Sleep(1000); // sleep 1 sec
+                }
+
+                if (dbConn.State == ConnectionState.Open)
+                    dbConn.Close();
+            }*/
+            #endregion
+            Console.WriteLine(String.Format("{0} - Model shutdown.", Helpers.getTimeStamp()));
         }
 
         // Singleton pattern <3
